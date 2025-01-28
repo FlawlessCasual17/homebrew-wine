671405cbe9e9dd12898804b9c7b45776a35bc64e
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-01-27 19:48:44 -0700

    order66: Update installation script again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- Formula/order66.rb
+++ Formula/order66.rb
@@ -10,12 +10,12 @@ class Order66 < Formula
         filename: "order66-v1.0.3-osx-x64.zip"
       sha256 "ce961d5072e8040e402e847789283b6e2f2bb9716a7c0821f041f3e22923304d"
 
-      def hash
+      def macos_hash
         stable.checksum.hexdigest if OS.macos?
       end
 
       def install
-        system "unzip", "#{hash}--#{filename}"
+        system "unzip", "#{macos_hash}--#{filename}"
         bin.install "Order66"
       end
     end
@@ -27,12 +27,12 @@ class Order66 < Formula
         filename: "order66-v1.0.3-linux-x64.zip"
       sha256 "08defa0fe2f7927d3fb9217a4815d884a5c0cd00b31115c68cb593767a276fee"
 
-      def hash
+      def linux_hash
         stable.checksum.hexdigest if OS.linux?
       end
 
       def install
-        system "unzip", "#{hash}--#{filename}"
+        system "unzip", "#{linux_hash}--#{filename}"
         bin.install "Order66"
       end
     end
