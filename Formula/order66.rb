d7a90ea65b4c0ccf299e0404b93e70f7443834f7
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-01-27 19:28:35 -0700

    order66: Update installation script.
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- Formula/order66.rb
+++ Formula/order66.rb
@@ -5,16 +5,28 @@ class Order66 < Formula
   license "AGPL-3.0"
 
   on_macos do
-    url "https://gitlab.com/olfek/order66/-/package_files/107183503/download"
-    sha256 "ce961d5072e8040e402e847789283b6e2f2bb9716a7c0821f041f3e22923304d"
+    if Hardware::CPU.is_64_bit?
+      url "https://gitlab.com/olfek/order66/-/package_files/107183503/download",
+        filename: "order66-v1.0.3-osx-x64.zip"
+      sha256 "ce961d5072e8040e402e847789283b6e2f2bb9716a7c0821f041f3e22923304d"
+
+      def install
+        system "unzip", "order66-v1.0.3-osx-x64.zip"
+        bin.install "Order66"
+      end
+    end
   end
 
   on_linux do
-    url "https://gitlab.com/olfek/order66/-/package_files/107183497/download"
-    sha256 "08defa0fe2f7927d3fb9217a4815d884a5c0cd00b31115c68cb593767a276fee"
-  end
+    if Hardware::CPU.is_64_bit?
+      url "https://gitlab.com/olfek/order66/-/package_files/107183497/download",
+        filename: "order66-v1.0.3-linux-x64.zip"
+      sha256 "08defa0fe2f7927d3fb9217a4815d884a5c0cd00b31115c68cb593767a276fee"
 
-  def install
-    bin.install "Order66"
+      def install
+        system "unzip", "order66-v1.0.3-linux-x64.zip"
+        bin.install "Order66"
+      end
+    end
   end
 end
