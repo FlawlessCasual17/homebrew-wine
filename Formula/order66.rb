9384623e1414484199c876d9d1431dd83022a098
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-01-27 22:45:09 -0700

    order66: Update installation script again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- Formula/order66.rb
+++ Formula/order66.rb
@@ -10,11 +10,9 @@ class Order66 < Formula
       sha256 "ce961d5072e8040e402e847789283b6e2f2bb9716a7c0821f041f3e22923304d"
 
       def install
-        Dir.glob('*.dll') do |libs|
-          lib.install libs
+        Dir.glob('*') do |binaries|
+          bin.install binaries
         end
-        lib.install "Order66.pdb"
-        bin.install "Order66"
       end
     end
   end
@@ -25,11 +23,9 @@ class Order66 < Formula
       sha256 "08defa0fe2f7927d3fb9217a4815d884a5c0cd00b31115c68cb593767a276fee"
 
       def install
-        Dir.glob('*.dll') do |libs|
-          lib.install libs
+        Dir.glob('*') do |binaries|
+          bin.install binaries
         end
-        lib.install "Order66.pdb"
-        bin.install "Order66"
       end
     end
   end
