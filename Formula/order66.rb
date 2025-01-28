555284401576ea36905e8c5de43e38bb372d85e3
FlawlessCasual17 <07e5297d5b@c0x0.com>
2025-01-27 22:09:57 -0700

    order66: Update installation script again
    Signed-off-by: FlawlessCasual17 <07e5297d5b@c0x0.com>

--- Formula/order66.rb
+++ Formula/order66.rb
@@ -10,8 +10,8 @@ class Order66 < Formula
       sha256 "ce961d5072e8040e402e847789283b6e2f2bb9716a7c0821f041f3e22923304d"
 
       def install
-        Dir.foreach('.') do |filename|
-          bin.install filename
+        Dir.glob('*.*') do |binary|
+          bin.install binary
         end
       end
     end
@@ -23,8 +23,8 @@ class Order66 < Formula
       sha256 "08defa0fe2f7927d3fb9217a4815d884a5c0cd00b31115c68cb593767a276fee"
 
       def install
-        Dir.foreach('.') do |filename|
-          bin.install filename
+        Dir.glob('*.*') do |binary|
+          bin.install binary
         end
       end
     end
